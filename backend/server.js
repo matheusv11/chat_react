3de064fd3bd9c89804a0668005301a01ac8f5f3f
matheusv11 <matheusv11@MacBook-Pro-de-matheusv11.local>
2020-05-19 23:44:44 -0300

    ok

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+    const express= require('express');
+    const app= express();
+
+    const server= require('http').createServer(app);
+    const io= require('socket.io')(server);
+
+  
+io.on('connection', socket => {
+
+    console.log(`socket conectado: ${socket.id}`);
+    
+    socket.on('chat.message', data => {
+        console.log('mensagem do chat => ', data)
+        io.emit('chat.message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Um user se desconectou');
+    });
+
+})
+
+
+//commit
+    // //Run quando client se conecta
+    // io.on('connection', (socket)=> {
+    //     console.log(`Socket conectado: ${socket.id}`);
+        
+    //     //Welcome do atual user
+    //     //socket.emit('message', 'Welcome');//Cliente especifico
+
+    //     //Quando um user menos voce se conectar
+    //     socket.broadcast.emit('message', 'Um user se conectou');//Todos clientes menos o conectado
+        
+    //     //Quando um user desconectar
+    //     socket.on('disconnect', ()=>{
+    //         io.emit('message', 'Um user saiu');
+    //     });
+        
+    //     //Mensagem do chat
+    //     socket.on('chatMessage', msg=>{
+    //         io.emit('message', msg);
+    //     });
+
+    //     //Mensagens passadas
+    //     // socket.on('previousMessage', msg=>{
+    //     //     io.emit('message', msg);
+    //     // })
+
+    //     //io.emit();//Todos clientes
+    // });
+
+    server.listen(3030);
+
+
+
